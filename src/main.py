08bfac01cb4105e49466537ff40c2cbada168a2e
jjunno <joonas.junno@protonmail.com>
2024-12-01 09:58:53 +0200

    debug blink led

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import player
+import led
 import time
 
 if __name__ == "__main__":
     player = player.Player()
-    player.play()
-    time.sleep(10)
-    player.stop()
+    led = led.Led()
+    led.on()
+    time.sleep(3)
+    led.off()
+    time.sleep(3)
+    led.on()
+    time.sleep(3)
+    led.off()
+    # player.play()
+    # time.sleep(10)
+    # player.stop()
