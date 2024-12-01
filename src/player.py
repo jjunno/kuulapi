6337e222c90c602b5f4bddde3bfea79fab629bfc
jjunno <joonas.junno@protonmail.com>
2024-12-01 12:14:25 +0200

    removed player state, unused

--- src/player.py
+++ src/player.py
@@ -5,22 +5,17 @@ class Player:
     self.audiofile = 'src/audiofile.mp3'
     pygame.mixer.init()
     pygame.mixer.music.load(self.audiofile)
-    self.state = False
     
   def play(self):
     print("Playing audio file")
-    self.state = True
     pygame.mixer.music.play()
     return True
   
   def stop(self):
     print("Stopping audio file")
-    self.state = False
     pygame.mixer.music.stop()
     return True
 
   def is_playing(self):
     busy = pygame.mixer.music.get_busy()
-    self.state = busy
-    # print(f"Player is busy: {busy}")
     return busy
